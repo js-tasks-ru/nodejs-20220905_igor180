53af5edaf70a84cc6ed02511ef1d78c0acec6153
Igor Kuryukin <kuryukinigor@gmail.com>
2022-09-21 21:25:19 +0300

    03-streams 02-line-split solution

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,22 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainder = '';
   }
 
   _transform(chunk, encoding, callback) {
+
+    const blocks = this.remainder.concat(chunk.toString()).split(os.EOL);
+
+    this.remainder = blocks.pop();
+    
+    blocks.forEach(element => this.push(element));
+    
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.remainder);
   }
 }
 
---
+++ 03-streams/02-line-split-stream/index.js
@@ -0,0 +1,20 @@
+const LineSplitStream = require('./LineSplitStream');
+const os = require('os');
+
+const lines = new LineSplitStream({
+  encoding: 'utf-8',
+});
+
+function onData(line) {
+  console.log(line);
+}
+
+lines.on('data', onData);
+
+lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
+// lines.write('a');
+//       lines.write(`b${os.EOL}c`);
+//       lines.write(`d${os.EOL}e`);
+//       lines.write('f');
+
+lines.end();
