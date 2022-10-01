bdfe35fb5d5b18332361f3d579d3d493b49b389e
Igor Kuryukin <kuryukinigor@gmail.com>
2022-10-01 19:01:00 +0300

    05-koajs  01-chat  solution

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,32 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+
+    const msg = await new Promise(resolve => {  // подвешиваем до получения сообщения
+        subscribers.push(resolve);              // складываем подписчиков==функции успешного окончания промисов
+    });
+
+    ctx.body = msg;  // после получения отдаем сообщение в ответе
+
 });
 
 router.post('/publish', async (ctx, next) => {
+
+    const msg = ctx.request.body.message;   // кто-то прислал сообщение
+
+    if (!msg) return;
+
+    subscribers.forEach((resolve) => {  // каждой функции успешного окончания промиса передадим как результат полученное сообщение
+        resolve(msg);  // здесь промис наконец выполнится и в тело ответа будет передано сообщение
+    });
+
+    //console.log(ctx.request.body);
+
+    ctx.body = 'ok';
+
 });
 
 app.use(router.routes());
